c82c97ad03f635486d011920f979e7bcd9a87348
Alejandro Guadarrama <alexgd@sni.com.mx>
2017-04-01 21:20:02 -0600

    grid

--- grid.py
+++ grid.py
@@ -1,14 +1,25 @@
+def print_pipe(x):
+    sqv = '| ' + '  ' * 4
+    sqev = '|'
+    print sqv * x + sqev
+
+def print_head(x):
+    sqh = '+ ' + '- ' * 4
+    sqeh = '+'
+    print sqh * x + sqeh
+
+def do_twice(p):
+    print_pipe(p)
+    print_pipe(p)
+
+def do_four(f,x):
+    f(x)
+    f(x)
+
 def grid(x,y):
     for yy in range(0 , y):
-        sqh = '+ '+'- '*4
-        sqeh = '+'
-        sqv = '| '+'  '*4
-        sqev = '|'
-        print sqh*x+sqeh
-        print sqv*x+sqev
-        print sqv*x+sqev
-        print sqv*x+sqev
-        print sqv*x+sqev
-    print sqh*x+sqeh
+        print_head(x)
+        do_four(do_twice,x)
+    print_head(x)
 
-grid(9,9)
+grid(4,4)
