4eee9a1aca02988248174c0d1b68bab442b0845a
Alejandro Guadarrama <alexgd@sni.com.mx>
2017-04-02 10:48:28 -0500

    add code

--- grid.py
+++ grid.py
@@ -1,25 +1,111 @@
-def print_pipe(x):
-    sqv = '| ' + '  ' * 4
-    sqev = '|'
-    print sqv * x + sqev
+"""This module contains code from
+Think Python by Allen B. Downey
+http://thinkpython.com
 
-def print_head(x):
-    sqh = '+ ' + '- ' * 4
-    sqeh = '+'
-    print sqh * x + sqeh
+Copyright 2012 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
-def do_twice(p):
-    print_pipe(p)
-    print_pipe(p)
+"""
 
-def do_four(f,x):
-    f(x)
-    f(x)
+# here is a mostly-straightforward solution to the
+# two-by-two version of the grid.
 
-def grid(x,y):
-    for yy in range(0 , y):
-        print_head(x)
-        do_four(do_twice,x)
-    print_head(x)
+def do_twice(f):
+    f()
+    f()
 
-grid(4,4)
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+
+def print_beam():
+    print '+ - - - -',
+
+def print_post():
+    print '|        ',
+
+def print_beams():
+    do_twice(print_beam)
+    print '+'
+
+def print_posts():
+    do_twice(print_post)
+    print '|'
+
+def print_row():
+    print_beams()
+    do_four(print_posts)
+
+def print_grid():
+    do_twice(print_row)
+    print_beams()
+
+print_grid()
+    
+
+# here is a less-straightforward solution to the
+# four-by-four grid
+
+def one_four_one(f, g, h):
+    f()
+    do_four(g)
+    h()
+
+def print_plus():
+    print '+',
+
+def print_dash():
+    print '-',
+
+def print_bar():
+    print '|',
+
+def print_space():
+    print ' ',
+
+def print_end():
+    print
+
+def nothing():
+    "do nothing"
+
+def print1beam():
+    one_four_one(nothing, print_dash, print_plus)
+
+def print1post():
+    one_four_one(nothing, print_space, print_bar)
+
+def print4beams():
+    one_four_one(print_plus, print1beam, print_end)
+
+def print4posts():
+    one_four_one(print_bar, print1post, print_end)
+
+def print_row():
+    one_four_one(nothing, print4posts, print4beams)
+
+def print_grid():
+    one_four_one(print4beams, print_row, nothing)
+
+print_grid()
+
+comment = """
+After writing a draft of the 4x4 grid, I noticed that many of the
+functions had the same structure: they would do something, do
+something else four times, and then do something else once.
+
+So I wrote one_four_one, which takes three functions as arguments; it
+calls the first one once, then uses do_four to call the second one
+four times, then calls the third.
+
+Then I rewrote print1beam, print1post, print4beams, print4posts,
+print_row and print_grid using one_four_one.
+
+Programming is an exploratory process.  Writing a draft of a program
+often gives you insight into the problem, which might lead you to
+rewrite the code to reflect the structure of the solution.
+
+--- Allen
+"""
+
+print comment
--- mypolygon.py
+++ mypolygon.py
@@ -1,5 +1,5 @@
 from swampy.TurtleWorld import *
-from math import sin, cos
+from math import sin, cos, pi
 
 world = TurtleWorld()
 bob = Turtle()
@@ -11,17 +11,18 @@ def square(t,l):
         lt(t)
 
 def polygon(t,l,n):
+    angle = 360.0/n
     for i in range(n):
         fd(t,l)
-        lt(t,360/n)
+        lt(t,angle)
 
 def circle(t,r):
-    n = 60.0
-    ai = 360/n
-    a = ai / 2.0
-    l = abs(2.0 * (sin(a) * r))
-    polygon(t,l,int(n))
+    circ = 2 * pi * r
+    n = int(circ/pi) +1
+    l = circ / n
+    polygon(t,l,n)
 
 #polygon(bob,75,12)
+bob.delay = 0.01
 circle(bob,100)
 wait_for_user()
